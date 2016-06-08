d2f8c04084207f532a5172caf968a7d3a25b8a8f
Robbert Broersma <mail@robbertbroersma.nl>
2016-06-09 01:02:22 +0200

    Update Sublime theme toggler to use themes from Git repos. #2

---
+++ setup-theme.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:altercation/solarized.git
+git clone git@github.com:deplorableword/textmate-solarized.git solarized-textmate
+git clone git@github.com:braver/Solarized.git solarized-sublime
--- theme.sh
+++ theme.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-DARK_THEME="Packages/User/SublimeLinter/Solarized (Dark) (SL).tmTheme"
-LIGHT_THEME="Packages/User/SublimeLinter/Xcode_default (SL).tmTheme"
+DARK_THEME=~/.dotfiles/solarized-sublime/Solarized\ \(dark\).tmTheme
+LIGHT_THEME=~/.dotfiles/solarized-sublime/Solarized\ \(light\).tmTheme
 
 SUBLIME_PREFS=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 TMP_PREFS=/tmp/Preferences.sublime-settings
 
-CURRENT_THEME=`jq -r '.color_scheme' "$SUBLIME_PREFS"`
+CURRENT_THEME=$(jq -r '.color_scheme' "$SUBLIME_PREFS")
 
-if [ "$CURRENT_THEME" = "$DARK_THEME" ]
+if [ "$(basename "$CURRENT_THEME")" == "$(basename "$DARK_THEME")" ]
 then
-	DARK_THEME="$LIGHT_THEME"
+	NEW_THEME="$LIGHT_THEME";
+else
+	NEW_THEME="$DARK_THEME";
 fi;
 
-jq --indent 2 '.color_scheme = $path' --arg path "$DARK_THEME" "$SUBLIME_PREFS" > /tmp/Preferences.sublime-settings
+# Since Sublime doesn't appear to support paths outside the Packages/User/ directory, generate relative paths:
+jq --indent 2 '.color_scheme = $path' --arg path "Packages/User/$(basename "$NEW_THEME")" "$SUBLIME_PREFS" > /tmp/Preferences.sublime-settings
 cp "$TMP_PREFS" "$SUBLIME_PREFS"
-
-
-jq -r '.color_scheme' ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
